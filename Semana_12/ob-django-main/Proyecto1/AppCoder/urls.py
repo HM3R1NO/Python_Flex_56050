6f4f318eb4006a33e8472e9bbf619b3bc777a454
ematevez <ematevez@gmail.com>
2023-11-14 12:43:05 -0300

    actualiza

---
+++ Semana_12/ob-django-main/Proyecto1/AppCoder/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from AppCoder.views import *
+
+
+
+urlpatterns = [
+    path('', inicio, name="home"),
+    path('cursos/', cursos, name="curioso"),
+    path('profesores/', profesores, name="profe"),
+    path('estudiantes/', estudiantes, name="estudiantes"),
+    path('entregables/', entregables, name='entregables'),
+    # path('curso/',curso,name='curso'),
+    
+    #Url de formularios 
+    path('cursoForm/',cursoForm, name='cursoForm'),
+    path('estudianteForm/',estudianteForm, name='estudianteForm'),
+    path('profesorForm/', profesorForm, name='profesorForm'),
+    
+    
+    #login
+    path('login/', login_view, name="Login"),
+    path('register/', register, name="Register"),
+    path('logout/', LogoutView.as_view(template_name='AppCoder/logout.html'), name='Logout'),
+]
